7273b7f34cf4d71bd50ac6d029a51cc4c83f2619
Priscilla <kayegipriscilla@gmail.com>
2026-05-21 14:21:15 +0300

    Conditional and ternary operators

---
+++ conditionals.py
@@ -0,0 +1,22 @@
+# temperature = 15
+
+# if temperature > 30:
+#     print("Its warm")
+# elif temperature > 20:
+#     print("Its nice")
+# else:
+#     print("Its cold")
+# print("Done")
+ 
+
+#Ternary operator
+# age = 15
+
+# message = "eligible" if age >= 18 else "Ineligible"
+
+# print(message)
+
+#Ternary operator with formatted string
+grade = 34
+
+print(f"You {'Passed' if grade >= 50 else 'Failed'} the exam")
